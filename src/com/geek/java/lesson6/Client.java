94a9292016da07631affd45d00a0d03201f2504d
Candida Larson <candida-latson@mail.ru>
2022-04-29 19:48:36 +0300

    Create server and client connections

--- src/com/geek/java/lesson6/Client.java
+++ src/com/geek/java/lesson6/Client.java
@@ -1,4 +1,15 @@
 package com.geek.java.lesson6;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 5664);
+            System.out.println("Client connected to the server");
+        } catch (IOException e) {
+            System.err.println("Client can not connect to the server");
+        }
+    }
 }
--- src/com/geek/java/lesson6/Server.java
+++ src/com/geek/java/lesson6/Server.java
@@ -1,4 +1,17 @@
 package com.geek.java.lesson6;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5664)) {
+            System.err.println("Start accept client connection");
+            serverSocket.accept();
+            System.err.println("Finish accept client connection");
+
+        } catch (IOException e) {
+            System.err.println("Server can not bind port");
+        }
+    }
 }
