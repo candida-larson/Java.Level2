c598e1a3775d525628f21bd486bdd0bcca13e190
Candida Larson <candida-latson@mail.ru>
2022-04-29 20:22:13 +0300

    Add readMessagesFromConsole and showClientMessages for Server class

--- src/com/geek/java/lesson6/Client.java
+++ src/com/geek/java/lesson6/Client.java
@@ -1,13 +1,28 @@
 package com.geek.java.lesson6;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 5664);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
             System.out.println("Client connected to the server");
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                String message = scanner.next();
+                dataOutputStream.writeUTF(message);
+
+                if (message.startsWith("/end")) {
+                    break;
+                }
+            }
+
         } catch (IOException e) {
             System.err.println("Client can not connect to the server");
         }
--- src/com/geek/java/lesson6/Server.java
+++ src/com/geek/java/lesson6/Server.java
@@ -1,17 +1,64 @@
 package com.geek.java.lesson6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
+    private static Socket socket;
+    private static DataInputStream dataInputStream;
+    private static DataOutputStream dataOutputStream;
+
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(5664)) {
-            System.err.println("Start accept client connection");
-            serverSocket.accept();
-            System.err.println("Finish accept client connection");
+            System.out.println("Start accept client connection");
+            socket = serverSocket.accept();
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Finish accept client connection");
+            showClientMessages();
+            readMessagesFromConsole();
 
         } catch (IOException e) {
             System.err.println("Server can not bind port");
         }
+    }
+
+    private static void readMessagesFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                String message = scanner.next();
+                dataOutputStream.writeUTF(message);
+                if (message.startsWith("/end")) {
+                    break;
+                }
+            } catch (IOException e) {
+                System.err.println("Cannot send message from server");
+                break;
+            }
+        }
+    }
+
+    private static void showClientMessages() {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                try {
+                    String message = dataInputStream.readUTF();
+                    System.out.println("> From client: " + message);
+                    if (message.startsWith("/end")) {
+                        break;
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error showClientMessage");
+                    break;
+                }
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
     }
 }
