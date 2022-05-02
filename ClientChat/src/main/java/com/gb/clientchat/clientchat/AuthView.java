c8595e99c98cb0947d659950fbcfe9f660f5c376
Candida Larson <candida-latson@mail.ru>
2022-05-02 17:44:18 +0300

    Create Auth view

---
+++ ClientChat/src/main/java/com/gb/clientchat/clientchat/AuthView.java
@@ -0,0 +1,4 @@
+package com.gb.clientchat.clientchat;
+
+public class AuthView {
+}
--- ClientChat/src/main/java/com/gb/clientchat/clientchat/ClientChatApplication.java
+++ ClientChat/src/main/java/com/gb/clientchat/clientchat/ClientChatApplication.java
@@ -3,20 +3,37 @@ package com.gb.clientchat.clientchat;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ClientChatApplication extends Application {
+    private Stage chatStage;
+    private Stage authStage;
+    private FXMLLoader authLoader;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ClientChatApplication.class.getResource("hello-view.fxml"));
+        this.chatStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientChatApplication.class.getResource("client-chat-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Client Chat");
         stage.setScene(scene);
         loadUserList(fxmlLoader);
         stage.centerOnScreen();
         stage.show();
+        initAuthDialog();
+    }
+
+    private void initAuthDialog() throws IOException {
+        authLoader = new FXMLLoader();
+        authLoader.setLocation(ClientChatApplication.class.getResource("auth-view.fxml"));
+        authStage = new Stage();
+        authStage.initOwner(chatStage);
+        authStage.initModality(Modality.WINDOW_MODAL);
+        authStage.setScene(new Scene(authLoader.load()));
+        authStage.show();
     }
 
     private void loadUserList(FXMLLoader fxmlLoader) {
